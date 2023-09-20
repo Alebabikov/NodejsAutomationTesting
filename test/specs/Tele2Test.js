1843d133603f89ccd82f7844ffe54c648576299c
Alexey Babikov <alex94.73rus@gmail.com>
2023-09-20 11:48:03 +0400

    add test, wdio, package

---
+++ test/specs/Tele2Test.js
@@ -0,0 +1,22 @@
+describe('Tele2 main page test',async()=>{                                                                              //describe it - описание теста с асинхронными функциями
+
+    it('Запускаю основную страницы Tele2-ЛК',async()=>{
+
+        await browser.url("https://nnov.tele2.ru/lk");                                                                  // подождет(await), пока запуститься браузер и страница, затем перейдет к следующему шагу
+        await expect(browser).toHaveUrl("https://nnov.tele2.ru/lk");                                                    // проверка, что url адрес соответствует указанному
+        await expect(browser).toHaveTitle("Личный кабинет Tele2 - вход по номеру телефона Нижегородская область");      // проверка заголовка вкладки
+        console.log(await browser.getTitle());                                                                          // выведет заголовок сайта, подождет, пока не напечатается полностью 
+      
+
+
+    });
+});
+
+
+/*
+//локаторы
+await $("//input[@id='keycloakAuth.phone']").click();
+await $("//input[@id='keycloakAuth.phone']").setValue('8005555555');  //ввод данных в поле
+await $('.not-print-element.close.icon-close').click();
+await browser.pause(2000);
+console.log(await $("//div[text()='Войдите в личный кабинет Tele2']").getText());   // получение текста из элемента */
